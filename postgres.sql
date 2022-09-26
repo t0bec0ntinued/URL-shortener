ef691b5d61fddc0d8c1e2619fd5674e74b26991e
t0bec0ntinued <112726936+t0bec0ntinued@users.noreply.github.com>
2022-09-26 20:36:09 +0300

    Create postgres.sql

---
+++ postgres.sql
@@ -0,0 +1,23 @@
+
+
+CREATE DATABASE db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+    CREATE TABLE IF NOT EXISTS public.mytable
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    input text COLLATE pg_catalog."default" NOT NULL,
+    output text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT mytable_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.mytable
+    OWNER to postgres;
