62ccbf3785d4e758bbfb0e1a6ea482795d99be2a
t0bec0ntinued <112726936+t0bec0ntinued@users.noreply.github.com>
2023-07-05 16:20:33 +0300

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -1,23 +1,21 @@
-
-
-CREATE DATABASE db
+CREATE DATABASE test_db
     WITH
-    OWNER = postgres
+    OWNER = test_user
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
-    CREATE TABLE IF NOT EXISTS public.mytable
+    CREATE TABLE IF NOT EXISTS public.testtb
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     input text COLLATE pg_catalog."default" NOT NULL,
     output text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT mytable_pkey PRIMARY KEY (id)
+    CONSTRAINT testtb_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.mytable
-    OWNER to postgres;
+ALTER TABLE IF EXISTS public.testtb
+    OWNER to test_user;
